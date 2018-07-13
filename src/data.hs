3b2a83d9047cd011fcb37b82e06c31c9c4e40db4
Dario Sindicic <dario.sindicic@gmail.com>
2018-07-13 09:46:04 +0200

    added iaaf data

---
+++ src/data.hs
@@ -0,0 +1,37 @@
+module Data where
+
+data Categories = M30
+                | M35
+                | M40
+                | M45
+                | M50
+                | M55
+                | M60
+                | M65
+                | M70
+                | M75
+                | M80
+                | M85
+                | M90
+                | F30
+                | F35
+                | F40
+                | F45
+                | F50
+                | F55
+                | F60
+                | F65
+                | F70
+                | F75
+                | F80
+                | F85
+                | F90
+                | F95
+
+data Disciplines = LONG_JUMP 
+                 | 100M
+                 | 200M
+                 | HIGH_JUMP
+                 | SHOT_PUT
+
+
